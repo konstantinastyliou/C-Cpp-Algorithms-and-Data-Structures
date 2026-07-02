d3f6c0df41ee8fe7105bee4bb4ad560b11456298
konstantinastyliou <konstantinastyliou@gmail.com>
2026-07-02 21:22:57 +0300

    Update Exercise2.c

--- Arrays_and_Matrices/Exercise2.c
+++ Arrays_and_Matrices/Exercise2.c
@@ -5,7 +5,7 @@
 
 
 
-typedef enum{FALSE,TRUE}Bool; // οριζω ετσι καινουργιο τυπο οπου χρειαζομαι
+typedef enum{FALSE,TRUE}Bool; 
 
 Bool is_perpedicular(float B[][N],int n);
 int main()
@@ -14,17 +14,15 @@ int main()
     int n,i,j;
 
     printf("Dwse to plithos n twn sthlwn\t");
-    scanf("%d",&n);  // αρχικα πρεπει να δωσουμε τις στηλες
-    while (n<1 || n>10){ /* περιοριζω ετσι αν ειναι κατω απο 1 ή πανω απο 10 να μου εκτυπώνει μηνυμα λαθους */
+    scanf("%d",&n);  
+    while (n<1 || n>10){ 
     printf("Dwse allh timh gia n");
     scanf("%d",&n);
     }
     for(i=0;i<n;i++)
         for(j=0;j<n;j++)
-      scanf("%f",&B[i][j]);  // διαβαζω στοιχεια για το Β
-    if (is_perpedicular(B,n)) /* με την βοήθεια της συνάρτησης θα ελεγχω
-                                 αν ενας πινακας ειναι ορθογώνιος ή οχι
-                                 και θα μου εμφανίζει το αντιστοιχο μηνυμα*/
+      scanf("%f",&B[i][j]);  
+    if (is_perpedicular(B,n)) 
         printf("The matrix is perpedicular");
     else
         printf("the matrix is NOT perpedicular");
@@ -42,9 +40,9 @@ int main()
             s+=B[k][i]*B[k][j];
             }
 
-            if (i==j && abs(1-s)>0.00005) // βαζω προϋποθέσεις ετσι ωστε να μπορω να εχω εναν ορθογώνιο πινακα
+            if (i==j && abs(1-s)>0.00005) 
                 return FALSE;
-            if (i!=j && abs(s)>0.00005)  //βαζω προϋποθέσεις ετσι ωστε να μπορω να εχω εναν ορθογώνιο πινακα
+            if (i!=j && abs(s)>0.00005) 
                 return FALSE;
     }
     return TRUE;
