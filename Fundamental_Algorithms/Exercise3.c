1c89c0c27768d1fde320e6fadd2a2dd5a2e4b250
konstantinastyliou <konstantinastyliou@gmail.com>
2026-07-02 21:21:41 +0300

    Update Exercise3.c

--- Fundamental_Algorithms/Exercise3.c
+++ Fundamental_Algorithms/Exercise3.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define PI 3.14159 //ορίζω σταθερες οπου θα χρειαστώ στη συνέχεια
+#define PI 3.14159 
 #define T 4
 #define w 2*PI/T
 
@@ -10,7 +10,7 @@
 int main()
 {
     int n,k;
-    float s,x,t; // αφου το Χ αποτελείται από float τότε και αυτός θα είναι float
+    float s,x,t; 
 
     printf("dwse gia timh n=");
     scanf("%d", &n);
@@ -18,13 +18,13 @@ int main()
     printf("dwse timh gia t=");
     scanf("%f", &t);
 
-    if (t<0)  /* ελέγχω την τιμή του t αν είναι αρνητικό τοτε θα εμφανιστεί μήνυμα λάθους */
+    if (t<0)  
         printf("Lathos timh gia to t\n");
     else {
-            s=0;  //αρχικοποιώ την τιμή που θα έχει το άθροισμα
-            for(k=0;k<n;k++) /*  με την χρήση της for το k θα ξειναει απο το 0 εως μεχρι k<n στη συνεχεια υπολογίζεται και μετα αυξάνεται κατα 1*/
-                s=s+pow(-1,k)*sin((2*k+1)*w*t)/(pow(2*k+1,2));  // εχω το αθροισμα s
-            x=8/pow(PI,2)*s;                                    // και εδω ειναι το αποτέλεσμα με την βοήθεια της s
+            s=0;  
+            for(k=0;k<n;k++) 
+                s=s+pow(-1,k)*sin((2*k+1)*w*t)/(pow(2*k+1,2));  
+            x=8/pow(PI,2)*s;                                   
             printf("n=%d t=%f  x=%f",n,t,x);
     }
 
