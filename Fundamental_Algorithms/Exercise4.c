3ff350d443cf7e09b25969681eb32f9297c98e53
konstantinastyliou <konstantinastyliou@gmail.com>
2026-07-02 21:22:07 +0300

    Update Exercise4.c

--- Fundamental_Algorithms/Exercise4.c
+++ Fundamental_Algorithms/Exercise4.c
@@ -7,8 +7,8 @@ int main()
 {
     int r;
     printf("dwse timh gia r:");
-    scanf("%d",&r); // δινω τιμη για το r οπου θα eiναι ο ορος που θα θελω να υπολογίσω
-    if (r<2)  // περιoρίζω το r ωστε κατω απο 2 να μου βγαζει μηνυμα λαθους
+    scanf("%d",&r); 
+    if (r<2)  
         printf("Lathos timh \n");
     else
         printf("a=%d",a(r));
@@ -16,10 +16,10 @@ int main()
     return 0;
 }
 int a(int r){
-    if (r==1) // η ασκηση μου δίνει το α1 ορο οποτε το χρησιμοποιω
+    if (r==1)
         return 1;
-    else if (r==0) // η ασκηση μου δίνει τον α0 ορο οποτε το χρησιμοποιω
+    else if (r==0) 
       return 1;
     else
-        return 5*a(r-1)-6*a(r-2)+pow(2,r)+r; /* η αναδρομικη συναρτηση που υπολογίζει τον r-στο ορο της ακολουθιας*/
+        return 5*a(r-1)-6*a(r-2)+pow(2,r)+r; 
 }
